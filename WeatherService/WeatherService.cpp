f886c20983cb86372af5ac6125edbbed6cba2c3f
LillyVanGogh <fgevilhtt@gmail.com>
2023-06-14 12:35:19 +0300

    working json stuff

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include "JsonService.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	JsonService js;
+	Weather w = js.getWeather("weather.json");
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
